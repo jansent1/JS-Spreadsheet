7747b822d56c811dac24757705a38e724f96ee1b
Teun Jansen <teunjansen91@gmail.com>
2024-04-09 12:22:44 +0200

    evalFormula v2.0 regex

--- script.js
+++ script.js
@@ -40,8 +40,34 @@ const charRange = (start, end) =>
 const evalFormula = (x, cells) => {
   const idToText = (id) => cells.find((cell) => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = (num) => (character) => idToText(character + num);
+  const addCharacters = (character1) => (character2) => (num) =>
+    charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(
+    rangeRegex,
+    (_match, char1, num1, char2, num2) =>
+      rangeFromString(num1, num2).map(addCharacters(char1)(char2))
+  );
+  const cellRegex = /[A-J][1-9][0-9]?/gi;
+  const cellExpanded = rangeExpanded.replace(cellRegex, (match) =>
+    idToText(match.toUpperCase())
+  );
 };
-
+/*const evalFormula = (x, cells) => {
+  const idToText = (id) => cells.find((cell) => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = (num) => {
+    const inner = (character) => {
+      return idToText(character + num);
+    };
+    return inner();
+  };
+  const addCharacters = (character1) => (character2) => (num) =>
+    charRange(character1, character2);
+};
+*/
 // The global window object represents the browser window (or tab)
 window.onload = () => {
   const container = document.getElementById("container");
